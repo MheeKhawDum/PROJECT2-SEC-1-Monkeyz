aa54a5624735a0ff81d5be3a66844f63153f9fbf
Shisanucha Chengsamo <110714943+isakokATP@users.noreply.github.com>
2024-10-17 21:52:59 +0700

    65130500016-[Update]-index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,64 +1,64 @@
 // router/index.js หรือไฟล์ router ของคุณ
-import { createRouter, createWebHistory } from 'vue-router'
-import HomePage from '@/components/HomePage.vue'
-import MenuPage from '@/components/MenuPage.vue'
-import CustomPage from '@/components/CustomPage.vue'
-import CartPopup from '@/components/CartPopup.vue'
-import EditPage from '@/components/EditPage.vue'
-import EditPageCustom from '../components/EditPageCustom.vue'
-import DrinkOptions from '@/components/DrinkOptions.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomePage from "@/components/HomePage.vue";
+import MenuPage from "@/components/MenuPage.vue";
+import CustomPage from "@/components/CustomPage.vue";
+import CartPopup from "@/components/CartPopup.vue";
+import EditPage from "@/components/EditPage.vue";
+import EditPageCustom from "../components/EditPageCustom.vue";
+import DrinkOptions from "@/components/DrinkOptions.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   //history: createWebHistory("/nw3/"),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomePage
+      path: "/",
+      name: "home",
+      component: HomePage,
     },
     {
-      path: '/menu',
-      name: 'menuPage',
+      path: "/menu",
+      name: "menuPage",
       component: MenuPage,
       // children: [
       //   {
       //     path: '/cart',
       //     name: 'cart',
-      //     component: CartPopup           
+      //     component: CartPopup
       //   }
       // ]
     },
     {
-      path: '/custom',
-      name: 'customPage',
-      component: CustomPage
+      path: "/custom",
+      name: "customPage",
+      component: CustomPage,
     },
     {
-      path: '/edit/:id',
-      name: 'edit',
-      component: EditPage
+      path: "/edit/:id",
+      name: "edit",
+      component: EditPage,
     },
     {
-      path: '/cart',
-      name: 'cart',
-      component: CartPopup           
+      path: "/cart",
+      name: "cart",
+      component: CartPopup,
     },
     {
-      path: '/drinkOption/:name',
-      name: 'drinkOption' ,
-      component: DrinkOptions
+      path: "/drinkOption/:name",
+      name: "drinkOption",
+      component: DrinkOptions,
     },
     {
-      path: '/editcustom/:id',
-      name: 'editCustom',
-      component: EditPageCustom
-    },    
+      path: "/editcustom/:id",
+      name: "editCustom",
+      component: EditPageCustom,
+    },
     {
-      path: '/',
-      redirect: {name: 'home'}
-    }
+      path: "/",
+      redirect: { name: "home" },
+    },
   ],
-})
+});
 
-export default router
+export default router;
