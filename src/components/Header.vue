289f5968033bef33d06dc9ab4685bbda4c680ad2
jittipat <jittipat349@gmail.com>
2024-10-18 21:42:44 +0700

    65130500009[ADD]show num of item on cart icon

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,17 @@
 <script setup>
 import { ref } from "vue";
 import { useRoute, useRouter } from "vue-router";
+import { getOrders } from "../lib/fetch";
 
 const router = useRouter();
+const cartCount = ref(); // Reactive variable for cart item count
+
+async function loadOrders() {
+  const orders = await getOrders();
+  cartCount.value = orders.length;
+}
+
+loadOrders();
 </script>
 
 <template>
@@ -68,7 +77,7 @@ const router = useRouter();
       <div class="flex-none">
         <slot name="cart">
           <button
-            class="btn btn-square btn-ghost bg-white rounded-xl"
+            class="btn btn-square btn-ghost bg-white rounded-xl relative"
             @click="router.push({ name: 'cart' })"
           >
             <svg
@@ -85,6 +94,7 @@ const router = useRouter();
                 d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
               />
             </svg>
+            <span v-if="cartCount" class="cart-count">{{ cartCount }}</span>
           </button>
         </slot>
       </div>
@@ -228,5 +238,16 @@ body {
   color: #333;
   text-align: center;
   margin-bottom: 20px;
+}
+.cart-count {
+  position: absolute;
+  top: -10px;
+  right: -10px;
+  background-color: #ff0000;
+  color: white;
+  border-radius: 50%;
+  padding: 5px 10px;
+  font-size: 0.8rem;
+  font-weight: bold;
 }
 </style>
